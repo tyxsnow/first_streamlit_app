ea81fe561ef7cda49bb27ed50cf8c7b3a43225c5
tyxsnow <127784511+tyxsnow@users.noreply.github.com>
2023-03-13 16:25:47 -0400

    Update streamlit_app.py
    Modification de la seconde ligne en header
    rajout de ligne de type Texte
    
    Rajout erreur typo pour voir le message d erreur

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthly Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
